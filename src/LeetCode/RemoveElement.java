2b78c46e620cbbdb66691ab060ee5b05e6205472
Ldv236 <Ldv236@yandex.ru>
2023-03-30 22:00:15 +0200

    RemoveElement

---
+++ src/LeetCode/RemoveElement.java
@@ -0,0 +1,40 @@
+package LeetCode;
+
+
+public class RemoveElement {
+
+    static int[] arr= {3,2,2,3};
+    static int val = 3;
+
+    static int[] arr2= {0,1,2,2,3,0,4,2};
+    static int val2 = 2;
+
+    static int res;
+
+    public static void main(String[] args) {
+        res = removeElement(arr, val);
+        res = removeElement(arr2, val2);
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int k = nums.length;
+        int temp;
+        int gap = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                temp = nums[nums.length - gap];
+                nums[nums.length - gap] = nums[i] ;
+                nums[i] = temp;
+                k--;
+                gap++;
+                if (nums[i] == val) {
+                    i--;
+                }
+            }
+            if (nums.length - gap == i) {
+                return  k;
+            }
+        }
+        return k;
+    }
+}
