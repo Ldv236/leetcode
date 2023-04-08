44b01884389ea7f405f454b93e7367001917f466
Ldv236 <Ldv236@yandex.ru>
2023-04-08 23:05:06 +0200

    SingleNumber2

--- src/LeetCode/Java/SingleNumber.java
+++ src/LeetCode/Java/SingleNumber.java
@@ -9,9 +9,9 @@ public class SingleNumber {
     static int[] nums3 = {1};
 
     public static void main(String[] args) {
-        System.out.println(singleNumber(nums1));
-        System.out.println(singleNumber(nums2));
-        System.out.println(singleNumber(nums3));
+        System.out.println(singleNumber2(nums1));
+        System.out.println(singleNumber2(nums2));
+        System.out.println(singleNumber2(nums3));
     }
 
     public static int singleNumber(int[] nums) {
@@ -23,5 +23,13 @@ public class SingleNumber {
             }
         }
         return nums[nums.length - 1];
+    }
+
+    public static int singleNumber2(int[] nums) {
+        int result = 0;
+        for(int i : nums) {
+            result ^= i;
+        }
+        return result;
     }
 }
