6db776f67033da64bcb47ff67df73902a5172952
Ldv236 <Ldv236@yandex.ru>
2023-04-15 18:48:00 +0200

    find disappeared 2

--- src/LeetCode/Java/FindAllNumbersDisappearedinArray.java
+++ src/LeetCode/Java/FindAllNumbersDisappearedinArray.java
@@ -6,12 +6,16 @@ public class FindAllNumbersDisappearedinArray {
 
     static int[] nums1 = {4,3,2,7,8,2,3,1};
     static int[] nums2 = {1,1};
-    static int[] nums3 = {1,2,4,4};
+    static int[] nums3 = {10,2,5,10,9,1,1,4,3,7};
 
     public static void main(String[] args) {
         System.out.println(findDisappearedNumbers(nums1));
         System.out.println(findDisappearedNumbers(nums2));
         System.out.println(findDisappearedNumbers(nums3));
+
+        System.out.println(findDisappearedNumbers2(nums1));
+        System.out.println(findDisappearedNumbers2(nums2));
+        System.out.println(findDisappearedNumbers2(nums3));
     }
 
     public static List<Integer> findDisappearedNumbers(int[] nums) {
@@ -26,6 +30,30 @@ public class FindAllNumbersDisappearedinArray {
             array.remove(Integer.valueOf(nums[i]));
         }
 
+        return array;
+    }
+
+    public static List<Integer> findDisappearedNumbers2(int[] nums) {
+        List<Integer> array = new ArrayList<>();
+        int n = nums.length;
+
+        nums = Arrays.stream(nums).sorted().toArray();
+
+        int addIndex = 1; //index that unique characters will be inserted at
+        for(int i = 0; i < n - 1; i++) {
+            if(nums[i] < nums[i + 1]){ //if true, num[i + 1] is a new unique number
+                nums[addIndex] = nums[i + 1];
+                addIndex++;
+            }
+        }
+
+        for (int i = 0, j = 1; (i < n && j <= n); j++) {
+            if (nums[i] == j) {
+                i++;
+            } else {
+                array.add(j);
+            }
+        }
         return array;
     }
 }
