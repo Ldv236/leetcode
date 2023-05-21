4d1b3aaeef26c71ec69748f57b359a13982ef0ba
Ldv236 <Ldv236@yandex.ru>
2023-05-21 08:29:26 +0200

    fix name's case

---
+++ src/LeetCode/SQL/FixNamesinaTable.sql
@@ -0,0 +1,22 @@
+/*Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and uppercase characters.
+
+
+Write an SQL query to fix the names so that only the first character is uppercase and the rest are lowercase.
+
+Return the result table ordered by user_id.*/
+
+SELECT [user_id],
+--UPPER(LEFT ([name], 1)) +
+    UPPER(SUBSTRING ([name], 1, 1)) +
+    LOWER(SUBSTRING ([name], 2, LEN([name]))) as [name]
+FROM [Users]
+ORDER BY user_id
