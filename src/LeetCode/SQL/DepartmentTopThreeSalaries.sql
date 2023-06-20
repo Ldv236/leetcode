dfca75a63742198c5b316d83ffe177ecbad53c39
Ldv236 <Ldv236@yandex.ru>
2023-06-20 22:08:33 +0200

    185

---
+++ src/LeetCode/SQL/DepartmentTopThreeSalaries.sql
@@ -0,0 +1,47 @@
+SELECT d.name AS Department
+     , r.name AS Employee
+     , r.salary AS Salary
+FROM
+    (SELECT name
+          , departmentId
+          , salary
+          , DENSE_RANK() OVER (PARTITION BY departmentId ORDER BY salary desc) AS rr
+     FROM Employee) AS r
+        JOIN Department AS d
+             ON d.id = r.departmentId
+WHERE r.rr <= 3
+
+/*
+ Table: Employee
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| id           | int     |
+| name         | varchar |
+| salary       | int     |
+| departmentId | int     |
++--------------+---------+
+id is the primary key column for this table.
+departmentId is a foreign key of the ID from the Department table.
+Each row of this table indicates the ID, name, and salary of an employee. It also contains the ID of their department.
+
+
+Table: Department
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of a department and its name.
+
+
+A company's executives are interested in seeing who earns the most money in each of the company's departments. A high earner in a department is an employee who has a salary in the top three unique salaries for that department.
+
+Write an SQL query to find the employees who are high earners in each of the departments.
+
+Return the result table in any order.
+ */
