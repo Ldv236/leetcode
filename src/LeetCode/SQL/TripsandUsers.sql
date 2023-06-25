3c628947c6ccd7803cae6b2d6b70f4d0f973680b
Ldv236 <Ldv236@yandex.ru>
2023-06-25 21:57:45 +0200

    262

---
+++ src/LeetCode/SQL/TripsandUsers.sql
@@ -0,0 +1,50 @@
+WITH unbanUsers AS (
+    SELECT users_id, role
+    FROM Users
+    WHERE banned = 'No'
+)
+SELECT request_at AS Day
+, ROUND(count(IF(status <> 'completed', 'ok', null))
+/ count(*), 2) AS "Cancellation Rate"
+FROM Trips
+WHERE client_id IN (select users_id from unbanUsers)
+  AND driver_id IN (select users_id from unbanUsers)
+GROUP BY Day
+having Day between '2013-10-01' and '2013-10-03'
+
+/*Table: Trips
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| id          | int      |
+| client_id   | int      |
+| driver_id   | int      |
+| city_id     | int      |
+| status      | enum     |
+| request_at  | date     |
++-------------+----------+
+id is the primary key for this table.
+The table holds all taxi trips. Each trip has a unique id, while client_id and driver_id are foreign keys to the users_id at the Users table.
+Status is an ENUM type of ('completed', 'cancelled_by_driver', 'cancelled_by_client').
+
+
+Table: Users
+
++-------------+----------+
+| Column Name | Type     |
++-------------+----------+
+| users_id    | int      |
+| banned      | enum     |
+| role        | enum     |
++-------------+----------+
+users_id is the primary key for this table.
+The table holds all users. Each user has a unique users_id, and role is an ENUM type of ('client', 'driver', 'partner').
+banned is an ENUM type of ('Yes', 'No').
+
+
+The cancellation rate is computed by dividing the number of canceled (by client or driver) requests with unbanned users by the total number of requests with unbanned users on that day.
+
+Write a SQL query to find the cancellation rate of requests with unbanned users (both client and driver must not be banned) each day between "2013-10-01" and "2013-10-03". Round Cancellation Rate to two decimal points.
+
+Return the result table in any order.*/
